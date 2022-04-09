f5b9c1ef2bbd97c6be09155194eb6b8e3603e837
Hello <2504788150@qq.com>
2022-04-09 16:53:23 +0800

    fourth commit

--- src/main/java/com/hello/community/controller/AuthorizeController.java
+++ src/main/java/com/hello/community/controller/AuthorizeController.java
@@ -5,6 +5,7 @@ import com.hello.community.dto.GithubUser;
 import com.hello.community.provider.GithubProvider;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +21,24 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callBack(@RequestParam("code")String code,
                            @RequestParam("state")String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("fd6e83c078c4d597be9a");
-        accessTokenDTO.setClient_secret("4d9d46c5582c6563d09e3355d4a4a810a44da51c");
+        System.out.println(clientId);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8889/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
--- src/main/java/com/hello/community/dto/AccessTokenDTO.java
+++ src/main/java/com/hello/community/dto/AccessTokenDTO.java
@@ -1,6 +1,7 @@
 package com.hello.community.dto;
 
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Data
 public class AccessTokenDTO {
